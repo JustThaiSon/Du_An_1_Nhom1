477c04ef2f37fa3ca7019aa758e4f55ed0ea6afc
JustThaiSon <hoangthaisonqs@gmail.com>
2024-07-14 00:01:53 +0700

    Database

---
+++ QuanLyPhong/DAL/Entities/Service.cs
@@ -0,0 +1,21 @@
+﻿using DAL.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entities
+{
+    public class Service
+    {
+        public Guid Id { get; set; }
+        public string? Name { get; set; }
+        public string? Descretion { get; set; }
+        public decimal Price { get; set; }
+        public ServiceStatus Status { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string? Type { get; set; }
+        public List<OrderService> OrderServices { get; set; }
+    }
+}
