aeb992e77525e39168b8cebff8e415ce9c67a441
Thao <Thaonmph47496@fpt.edu.vn>
2024-07-18 15:59:22 +0700

    cap nhap employSevice

--- QuanLyPhong/BUS/IService/IEmployeeService.cs
+++ QuanLyPhong/BUS/IService/IEmployeeService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DAL.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace BUS.IService
 {
     internal interface IEmployeeService
     {
+        string AddEmployee(Employee Employee);
+        string RemoveEmployee(Guid Id);
+        string UpdateEmployee(Employee Employee);
+        List<Floor> GetAllEmployeeFromDb();
     }
 }
